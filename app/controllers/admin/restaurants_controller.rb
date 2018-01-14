84f16545028bb34ba90767120f1823fcdf87dbd2
frank402 <frank40202@gmail.com>
2018-01-15 05:18:50 +0800

    add restaurant show action and view for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -2,7 +2,6 @@ class Admin::RestaurantsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
 def index
-  end
 @restaurants = Restaurant.all #這個 Action 的目的是要顯示全部餐廳的資料。
 end
 
