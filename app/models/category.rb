d462b4722695861864d10204ac3d96d8dad9100e
frank402 <frank40202@gmail.com>
2018-03-11 14:51:51 +0800

    styled feeds page & add dependency between models & user can view recent restaurants and comments on feeds page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
-	has_many :restaurants
+	  # 如果分類下已有餐廳，就不允許刪除分類（刪除時拋出 Error）
+  has_many :restaurants, dependent: :restrict_with_error
 
 
 	validates_presence_of :name #確保分類名稱必填
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-	has_many :comments
+
 	validates_presence_of :name
   mount_uploader :avatar, PhotoUploader
+
+    # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error）
+  has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
   
    # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
