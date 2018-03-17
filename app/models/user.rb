6cb1ea97ce666f1b9e227c7327cf2ff90005678a
frank402 <frank40202@gmail.com>
2018-03-17 21:42:41 +0800

    create favorite model & setup relation among user, restaurant and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,10 @@ class User < ApplicationRecord
   has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
   
+  # 「使用者收藏很多餐廳」的多對多關聯
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
+  
    # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
     self.role == "admin"
