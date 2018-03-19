d4f6f1d00c824756bdacddb5654e76b575079c17
frank402 <frank40202@gmail.com>
2018-03-19 12:41:44 +0800

    add favorite / unfavorite action on RestaurantsController

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,14 @@ class User < ApplicationRecord
 	validates_presence_of :name
   mount_uploader :avatar, PhotoUploader
 
-    # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error）
+    # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error） , 「使用者評論很多餐廳」的多對多關聯
   has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
   
   # 「使用者收藏很多餐廳」的多對多關聯
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
-  
+
    # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
     self.role == "admin"
