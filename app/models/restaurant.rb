3d222d405af5657b220752e59d089c4a8caa82ea
frank402 <frank40202@gmail.com>
2018-03-22 15:31:19 +0800

    create like model & setup relation among user,restaurant and like

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,9 +4,13 @@ class Restaurant < ApplicationRecord
 	mount_uploader :image, PhotoUploader
 	belongs_to :category
 	has_many :comments, dependent: :destroy
-	  has_many :favorites, dependent: :destroy
+	has_many :favorites, dependent: :destroy
 	has_many :favorited_users, through: :favorites, source: :user
 	
+  # 「使用者喜歡很多餐廳」的多對多關聯
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
+
 	#定義是否被收藏過,在 Class 裡，
 	# 使用 self 來代表 instance 本身，並使用關聯方法 self.favorited_users 
 	# 查出該餐廳物件相關的所有 User 紀錄。
