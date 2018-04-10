9e269b6c40f9467fccd848cc7e76e331795897a8
frank402 <frank40202@gmail.com>
2018-04-11 06:17:10 +0800

    add unfollow button on user index page and destroy action

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -4,5 +4,7 @@ class Followship < ApplicationRecord
   # 使用 :class_name 告知對應的 Model 名稱
   belongs_to :user
   belongs_to :following, class_name: 'User'
+  
+  # 這個驗證透過對 Model 的資料表執行一條 SQL 查詢語句，搜尋是否已經有同樣數值的紀錄存在。:scope 選項可以用另一個屬性來限制唯一性：
   validates :following_id, uniqueness: { scope: :user_id }
 end
