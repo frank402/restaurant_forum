348374435b8ee6802cd870df3077e9ae07132545
frank402 <frank40202@gmail.com>
2018-04-15 04:34:40 +0800

    setup self-referential relationships within users table through friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,10 @@ class User < ApplicationRecord
   has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
   has_many :followers, through: :inverse_followships, source: :user
 
+  #
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
   # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
     role == 'admin'
